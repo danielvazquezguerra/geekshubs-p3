767f88fc869a97f1528c14a1404de47b5edeeecd
danielvazquezguerra <daniel.vazquezguerra@gmail.com>
2020-03-10 23:14:18 +0100

    fetch conectado

--- scripts/buscador.js
+++ scripts/buscador.js
@@ -1,1 +1,9 @@
 console.log('hola');
+
+
+
+fetch('https://api.themoviedb.org/3/movie/top_rated?api_key=f213488ae3f21d01f82fcbb8d801c399&language=es-ESquery=')
+.then(res => res.json())
+.then(res => {
+    console.log(res.results[0].title);
+});
