0ba6c2d88a9dac6f0a2f50311fb91496e6e111af
danielvazquezguerra <daniel.vazquezguerra@gmail.com>
2020-03-11 18:28:41 +0100

    grid completado, creando los bloques de pelis.

--- scripts/buscador.js
+++ scripts/buscador.js
@@ -1,13 +1,23 @@
-let busqueda = 'bad boys';
+let busqueda = 'el mago de oz';
 
 fetch('https://api.themoviedb.org/3/search/movie?api_key=f213488ae3f21d01f82fcbb8d801c399&query='+busqueda)
+
 .then(res => res.json())
 .then(res => {
 
     let peliculas = res.results;
 
-    for (let pelicula of peliculas){
+    for (let pelicula of peliculas) {
+        
         console.log(pelicula.poster_path);
+
+        let container = document.querySelector('.container');
+        let peli = document.createElement('article');
+        container.append(peli);
+        console.log(peli);
+        
+
+
     }
     
 
