68918cdf09c211bd7e129b79fbc1f23d0366e9d6
danielvazquezguerra <daniel.vazquezguerra@gmail.com>
2020-03-13 00:51:09 +0100

    foto subiendo al dom

--- scripts/buscador.js
+++ scripts/buscador.js
@@ -4,6 +4,7 @@ const baseUrl = 'https://api.themoviedb.org/3/search/movie?';
 let busqueda = 'amor';
 const lista = '&language=en-US&query=';
 let rootFetch = baseUrl+key+lista+busqueda;
+let container = document.querySelector('.container');
 
 
 
@@ -11,4 +12,15 @@ axios.get(rootFetch)
 .then(res =>{
     let peliculas = res.data.results
     console.log(peliculas)
+
+    for(pelicula of peliculas) {
+
+        let rutaImg = 'https://image.tmdb.org/t/p/w500'+pelicula.poster_path;
+        console.log(pelicula)
+       
+        container.innerHTML = '<div><img class="fotopeli" src='+rutaImg+'></div>';
+
+    }
+
+    
 })
