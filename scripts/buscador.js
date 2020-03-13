e9a43036069c1baba3efc1f19783f7f12d6bb762
danielvazquezguerra <daniel.vazquezguerra@gmail.com>
2020-03-13 01:30:56 +0100

    for or modificado

--- scripts/buscador.js
+++ scripts/buscador.js
@@ -1,7 +1,7 @@
 
 const key = 'api_key=f213488ae3f21d01f82fcbb8d801c399';
 const baseUrl = 'https://api.themoviedb.org/3/search/movie?'; 
-let busqueda = 'amor';
+let busqueda = 'bad boys II';
 const lista = '&language=en-US&query=';
 let rootFetch = baseUrl+key+lista+busqueda;
 let container = document.querySelector('.container');
@@ -12,13 +12,13 @@ axios.get(rootFetch)
 .then(res =>{
     let peliculas = res.data.results
     console.log(peliculas)
-
-    for(pelicula of peliculas) {
-
-        let rutaImg = 'https://image.tmdb.org/t/p/w500'+pelicula.poster_path;
-        console.log(pelicula)
-       
-        container.innerHTML = '<div><img class="fotopeli" src='+rutaImg+'></div>';
+    
+    for (pelicula of peliculas) {
+        
+        console.log(pelicula.title);
+        let rutaImg = 'https://image.tmdb.org/t/p/w300'+pelicula.poster_path;
+        container.innerHTML = '<div><img class="fotopeli" src='+ rutaImg +'></div>'
+        
 
     }
 
