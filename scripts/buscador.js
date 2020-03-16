45aad3d52bb7d57de9f1ab5728e073b88394897e
danielvazquezguerra <daniel.vazquezguerra@gmail.com>
2020-03-16 17:36:27 +0100

    buscador funcionando

--- scripts/buscador.js
+++ scripts/buscador.js
@@ -1,32 +1,44 @@
 
-const key = 'api_key=f213488ae3f21d01f82fcbb8d801c399';
-const baseUrl = 'https://api.themoviedb.org/3/search/movie?'; 
-let busqueda = 'bad boys';
-const lista = '&language=en-US&query=';
-let rootFetch = baseUrl+key+lista+busqueda;
-let container = document.querySelector('.container-grid');
-let containerUp = document.querySelector('.container-up');
-let img = document.createElement('img');
-let imgBusqueda = document.querySelector('.imgBusqueda');
-const imgRootBase = `https://image.tmdb.org/t/p/w500`;
 
 
-axios.get(rootFetch)
-.then(res => {
-    let peliculas = res.data.results;
-    peliSlice = peliculas.slice(1,6);
-    let peliFondo = peliculas[0];
+
+document.querySelector('#buscar').addEventListener('click', ()=>{
+    
+    let container = document.querySelector('.container-grid');
+    container.innerHTML = "";
+
+    const baseUrl = 'https://api.themoviedb.org/3/search/movie?';
+    const key = 'api_key=f213488ae3f21d01f82fcbb8d801c399';
+    const lista = '&language=en-US&query=';
+    let input = document.querySelector('#input-busqueda').value;
+    let busqueda = input;
+    let rootFetch = baseUrl+key+lista+busqueda;
+    
+   
+    axios.get(rootFetch)
+
+    .then(res => {
+
+        let peliculas = res.data.results;
+        peliSlice = peliculas.slice(0,6);
+        let peliFondo = peliculas[0];
 
     console.log(peliculas)
 
-    imgBusqueda.innerHTML = `<img class='img-busqueda' src ='${imgRootBase}${peliculas[0].poster_path}')>`;
+    
+    let containerUp = document.querySelector('.container-up');
+    let img = document.createElement('img');
+    let imgBusqueda = document.querySelector('.imgBusqueda');
+    
+    console.log(input);
+    const imgRootBase = `https://image.tmdb.org/t/p/w500`;
+    
 
+    imgBusqueda.innerHTML = `<img class='img-busqueda' src ='${imgRootBase}${peliculas[0].poster_path}')>`;
 
     
     for (pelicula of peliSlice) {
         
-
-        console.log(pelicula.title);
 
         if (pelicula.poster_path != null) {
 
@@ -34,6 +46,8 @@ axios.get(rootFetch)
             container.innerHTML += `<div class="fotopeli"><img class="foto-down" id='img-index' src="${rutaImg}"></div>` 
     
         }
+
+        
         
        
     }
@@ -41,6 +55,10 @@ axios.get(rootFetch)
     containerUp.style.background = `url('${imgRootBase}${peliFondo.poster_path}')`;
     containerUp.style.backgroundSize = 'cover';
     containerUp.style.background.filter = 'grayscale(100%)';
+
+
+})
+
 
 
 })
